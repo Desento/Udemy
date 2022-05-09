ad58f22082041001a1af15ed23a1199114722641
Anton <astapenko91@gmail.com>
2022-05-09 14:36:08 +0300

    feat: add solution for task[main]

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const numberOfFilms = +prompt('Why mach films did you see?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+const a = prompt('One of the last movies seen?', ''),
+      b = prompt('How much would you rate it?', ''),
+      c = prompt('One of the last movies seen?', ''),
+      d = prompt('How much would you rate it?', '');
+
+personalMovieDB.movies[a] = b;      
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
