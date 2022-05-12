b369f6e821552b962907ffc50281169b3cda6a4d
Anton <astapenko91@gmail.com>
2022-05-12 21:18:24 +0300

    fix: code style of index.js case 4[main]

---
+++ 13.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    return console.log(str.split(' ').reverse());
+}
+reverse(someString);
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+
+}
